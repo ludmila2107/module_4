bb659db5d5441c1f9a92ba11aa46ba6e9f00628f
Ludmila <lvminzulina@mail.ru>
2024-08-07 20:24:21 +0300

    module_4.1-4.2.

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from true_math import divide as divide1
+from fake_math import divide as divide2
+
+result1 = divide1(69, 3)
+result2 = divide1(3, 0)
+result3 = divide2(49, 7)
+result4 = divide2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
